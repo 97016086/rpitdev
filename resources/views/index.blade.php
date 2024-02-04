7c6c6929102b3b93d00dc0d5a15cffbf57964cc5
Renate <97016086@st.deltion.nl>
2024-02-04 21:41:23 +0100

    made a dark mode
    made a component in which i made a dark mode for the website. also updated the app.css and the app.js files for alpine.js

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,12 +3,16 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  @vite(['resources/css/app.css'])
+  <title>Home</title>
+  @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
+
 <body x-data="{darkMode:  false}" :class="{'dark':  darkMode  === true}" class="antialiased">
-  <div class="max-w-7xl mx-auto p-6 lg:p-8">
+ <nav class="bg-slate-200 shadow mb-10">
+  <div class="max-w-3xl mx-auto py-3 px-6 md:px-0 md:flex md:justify-between">
     <x-theme-toggle/>
   </div>
+</nav>
 
 </body>
 
