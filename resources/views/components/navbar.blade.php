aac4362027d8c6f7d3f8bfd58992b4f0ee1ecd31
Renate <97016086@st.deltion.nl>
2024-02-21 10:54:15 +0100

    changed the navbar
    changed the navigation bar style again.

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,52 +1,32 @@
-<nav class=" bg-slate-300 dark:bg-sky-900 ">
-  <div class="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
-    <div class="relative flex h-16 items-center justify-between">
-      <div class="absolute inset-y-0  flex items-center sm:hidden">
-        <!--Mobile menu button-->
-        <button type="button" class="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white" aria-controls="mobile-menu" aria-expanded="false">
-          <span class="absolute -inset-0.5"></span>
-          <span class="sr-only">Open main menu</span>
-          <!--
-          Icon when menu is closed.
-            Menu open: "hidden", Menu closed: "block"
-          -->
-          <svg class="block h-6 w-6" fill="none" viewBox="0 0 24 24"  stroke-width="1.5" stroke="currentColor" aria-hidden="true">
-            <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75  12h16.5m-26.5 5.25h16.5"  />
-          </svg>
-          <!-- Icon when menu is open.
-            Menu open: "block", Menu closed: "hidden"
-          -->
-          <svg class="hidden h-6  w-6"  fill='none' viewBoox="0 0 24  24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
-            <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
-          </svg>
-        </button>
-      </div>
-      <div class="flex flex-1 items-center justify-center sm:items-stretch sm:justfy-start">
-        <div class="flex flex-shrink-0 items-center">
-          <x-theme-toggle />
-        </div>
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale())  }}">
 
-        <div class="hidden sm:ml-6 sm:block">
-          <div class="flex space-x-3">
-             <!--Current: "bg-slate-400 text-white", Default: "text-gray-300 hover: bg-slate:500 hover:text-white" -->
-             <a href="/" class=  "bg-slate-400 dark:bg-sky-950 rounded-md px-3 py-2 text-sm font-medium dark:text-white" aria-current="page">Home</a>
-             <a href="/aboutme" class= " block rounded-md px-3  py-2  text-sm font-medium dark:text-white">About Me</a>
-             <a href="#" class= "rounded-md px-3  py-2  text-sm font-medium dark:text-white">Projects</a>
-             <a href="#" class= "rounded-md px-3  py-2  text-sm font-medium dark:text-white">Contact</a>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>@yield('title')</title>
+  @vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
+
+<body>
+<div class="w-full	text-gray-700 bg-slate-300	dark:text-white	dark:bg-sky-900" >
+	<div	x-data="{	open:	false	}"	 class="flex flex-col  px-4 mx-auto md:items-center md:justify-between md:flex-row md:px-6 lg:px-8">
+		<div class="p-4	flex	flex-rowitems-center	justify-between">
+			<x-theme-toggle	/>
+			<button class="md:hidden rounded-lg focus:outline-none focus:shadow-outline" @click="open = !open">
+				<svg fill="currentColor" viewBox="0 0 20 20" class="w-6 h-6">
+					<path x-show="!open" fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM9 15a1 1 0 011-1h6a1 1 0 110 2h-6a1 1 0 01-1-1z" clip-rule="evenodd"></path>
+          <path x-show="open" fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path>
+				</svg>
+			</button>
+		</div>
+<nav	:class="{'flex':	open, 'hidden':	!open}"	class="flex-col flex-grow	pb-4	md:pb-0	hidden	md:flex	md:justify-end md:flex-row">
+	<a	class="px-4 py-2	mt-2	text-sm	font-semibold	text-gray-900	bg-slate-400 rounded-lg dark:bg-slate-500 dark:hover:bg-slate-600 dark:focus:bg-slate-600 dark:text-white md:ml-4 hover:bg-slate-500 hover:text-gray-700 active:bg-slate-600	focus:outline-none	focus:shadow-outline"	href="/">Home</a>
+	<a	class="px-4 py-2	mt-2	text-sm	font-semibold	text-gray-900	bg-slate-400 rounded-lg dark:bg-slate-500 dark:hover:bg-slate-600 dark:focus:bg-slate-600 dark:text-white md:ml-4 hover:bg-slate-500 hover:text-gray-700	active:bg-slate-600	focus:outline-none	focus:shadow-outline"	href="/aboutme">About me</a>
+	<a	class="px-4 py-2	mt-2	text-sm	font-semibold	text-gray-900	bg-slate-400 rounded-lg dark:bg-slate-500 dark:hover:bg-slate-600 dark:focus:bg-slate-600 dark:text-white md:ml-4 hover:bg-slate-500 hover:text-gray-700	active:bg-slate-600	focus:outline-none	focus:shadow-outline"	href="/portfolio">Portfolio</a>
+	<a	class="px-4 py-2	mt-2	text-sm	font-semibold	text-gray-900	bg-slate-400 rounded-lg dark:bg-slate-500 dark:hover:bg-slate-600 dark:focus:bg-slate-600 dark:text-white md:ml-4 hover:bg-slate-500 hover:text-gray-700	focus:outline-none	focus:shadow-outline"	href="/contact">Contact</a>	
+				</div>		
+		</div>
+	</nav>
 </div>
-        <!-- Mobile menu, show/hode based on menu state. -->
-        <div class="sm:hidden" id="mobile-menu">
-         <div class="space-y-1 px-2 pb-3 pt-2">
-          <!--Current: "bg-slate-400 text-white", Default: "text-gray-300 hover: bg-slate:500 hover:text-white" -->
-          <a href="/" class=  "bg-slate-400 dark:bg-sky-950 block rounded-md px-3 py-2  text-base font-medium dark:text-white" aria-current="page">Home</a>
-          <a href="/aboutme" class= " block rounded-md px-3  py-2  text-base font-medium dark:text-white">About Me</a>
-          <a href="#" class= " block rounded-md px-3  py-2  text-base font-medium dark:text-white">Projects</a>
-          <a href="#" class= "block rounded-md px-3  py-2  text-base font-medium dark:text-white">Contact</a>  
-    </div> 
-  </div>   
-</nav>
+</body>
---
+++ resources/views/portfolio.blade.php
@@ -0,0 +1,5 @@
+@extends('layout.layout')
+
+<head>
+  <title>portfolio</title>
+</head>
